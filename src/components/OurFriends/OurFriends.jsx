7893000fbc96902e20eef1db659e8684feda1ce8
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-02-01 21:46:35 +0200

    corrected

--- src/components/OurFriends/OurFriends.jsx
+++ src/components/OurFriends/OurFriends.jsx
@@ -7,22 +7,22 @@ export const OurFriends = ({ items }) => {
             <ul>
               {items && items.map(({ _id, title, url, addressUrl, imageUrl, address, workDays, phone, email }) =>
                 <li key={_id}>
-                    <a href={url} target="_blanck"><img width="100" src={`${imageUrl}`} alt="" /></a>
+                    <a rel="noreferrer" href={url} target="_blank"><img width="100" src={`${imageUrl}`} alt="" /></a>
                     <h2>{title}</h2>
                     <p>Time:</p>  
                     <ul>
-                        {workDays ? workDays.map(({ isOpen, from, to }) => from && <li>{from} - {to}</li>) : "-----------------------------------"}
+                        {workDays ? workDays.map(({ isOpen, from, to }, index) => from && <li key={index}>{from} - {to}</li>) : "-----------------------------------"}
                     </ul>
                       
                   <address>
                     <p>Address: </p>
-                    {address ? <p><a href={addressUrl} target="_blanck" >{address}</a></p> : "-----------------------------------"}
+                    {address ? <p><a href={addressUrl}  rel="noreferrer" target="_blank" >{address}</a></p> : "-----------------------------------"}
 
                     <p>Email: </p>
-                    {email ? <p><a href={email} target="_blanck" >{email}</a></p> : "-----------------------------------"}
+                    {email ? <p><a href={`mailto:${email}`}  rel="noreferrer" target="_blank" >{email}</a></p> : "-----------------------------------"}
 
                     <p>Phone: </p>
-                    {phone ? <p><a href={phone} target="_blanck" >{phone}</a></p> : "-----------------------------------"}
+                    {phone ? <p><a href={`tel:${phone}`}  rel="noreferrer" target="_blank" >{phone}</a></p> : "-----------------------------------"}
                   </address>
                 </li>)}
             </ul>
