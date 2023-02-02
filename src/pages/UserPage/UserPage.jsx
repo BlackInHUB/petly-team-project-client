287b4badfef5462bdcb1ee15b6ee264c76b2e13b
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2023-02-02 15:23:35 +0200

    add UserPage

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,23 +1,19 @@
-import { UserData } from "components/UserData/UserData";
-import { Logout } from "components/Logout/Logout";
-import { UserDataTitle } from "components/UserDataTitle/UserDataTitle";
-import { PetsData } from "components/PetsData/PetsData";
-import { UserPageWrapper, UserDataLogoutContainer } from './UserPage.styled'
+import { Logout } from "components/Logout/Logout"
+import { PetsData } from "components/PetsData/PetsData"
+import { UserData } from "components/UserData/UserData"
+import { UserDataTitle } from "components/UserDataTitle/UserDataTitle"
+import { UserDataLogoutContainer, UserPageWrapper } from "./UserPage.styled"
 
-const UserPage = () => {
-    return(
-        <>
+
+export const UserPage = () => {
+    return (
         <UserPageWrapper>
-            <UserDataTitle title='My information:' />
+            <UserDataTitle title='My information:'/>
             <UserDataLogoutContainer>
                 <UserData />
                 <Logout />
             </UserDataLogoutContainer>
-            {/* <UserDataTitle title='My pets:' /> */}
             <PetsData />
         </UserPageWrapper>
-        </>
     )
 }
-
-export default UserPage;
