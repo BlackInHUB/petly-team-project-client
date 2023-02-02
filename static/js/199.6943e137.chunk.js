0282bcede36e3832f3b96287543ecdc057291210
BlackInHUB <96769199+BlackInHUB@users.noreply.github.com>
2023-02-02 15:17:21 +0000

    Deploying to gh-pages from @ BlackInHUB/petly-team-project-client@b252565d37500b0c8fb78361cbc176cf781925be 🚀

--- static/js/199.6943e137.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[199],{3199:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(184);function a(){return(0,n.jsx)("h1",{children:"NewsPage page"})}}}]);
-//# sourceMappingURL=199.6943e137.chunk.js.map
