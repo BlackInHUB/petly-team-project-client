79f21bd9e5f34eb6aeda8ec6fda7a76fe73218a3
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-03 21:34:48 +0200

    Update noticesOperations.js

--- src/redux/notices/noticesOperations.js
+++ src/redux/notices/noticesOperations.js
@@ -7,5 +7,35 @@ const getAll = createAsyncThunk('notices/getAll', async (category, thunkApi) => 
         
     } catch (error) {
         return thunkApi.rejectWithValue(error.response.message);
-    }
+    };
+});
+
+const getOne = createAsyncThunk('notices/getOne', async (id, thunkApi) => {
+    try {
+        const result = await api.getOne(id);
+
+    } catch (error) {
+        console.log(error);
+        return thunkApi.rejectWithValue(error.response.message);
+    };
+});
+
+const getOwn = createAsyncThunk('notices/getOwn', async (_, thunkApi) => {
+    try {
+        const result = await api.getOwn();
+        
+    } catch (error) {
+        console.log(error);
+        return thunkApi.rejectWithValue(error.response.message);
+    };
+});
+
+const getFavorites = createAsyncThunk('notices/getFavorites', async (_, thunkApi) => {
+    try {
+        const result = await api.getFavorites();
+
+    } catch (error) {
+        console.log(error);
+        return thunkApi.rejectWithValue(error.response.message);
+    };
 })
