99a4b89ef2d75771a710b4198476554706bccad0
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-03 19:49:04 +0200

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: 'https://petly.onrender.com/api'
 });
 
 const setToken = (token) => {
