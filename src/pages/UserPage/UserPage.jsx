cd900270109ff4943bb753aca2f87074a6e006eb
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-04 09:47:35 +0200

    Update UserPage.jsx

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -7,6 +7,7 @@ import { UserDataContainer, UserPageWrapper } from "./UserPage.styled"
 
 const UserPage = () => {
     return (
+        <>
         <UserPageWrapper>
             <UserDataTitle title='My information:'/>
             <UserDataContainer>
