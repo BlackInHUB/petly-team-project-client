256e6b98f2612c518f359d7b145236d72ef23aa5
NataliiaKolomiichenko <gella1996nr@gmail.com>
2023-02-05 18:36:46 +0200

    ModalAddNotice

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,7 +1,13 @@
+import AddNoticeForm from 'components/AddNoticeForm/AddNoticeForm'
+
 export default function NoticesPage() {
+  
   return (
+    <>
       <h1>
         NoticesPage page
       </h1>
+      <AddNoticeForm />
+    </>
   );
 }
