17f0826b6f06633321835499e805c83b8d7cb46a
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-02-10 23:22:19 +0200

    add button click

--- src/components/OurFriends/TimeContainer.jsx
+++ src/components/OurFriends/TimeContainer.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import { Time, TimeList, LiStyled, ListWeek, ListTime } from "./TimeContainerStyled";
 
 export const date = new Date();
 
 const week = ["MN", "TU", "WE", "TH", "FR", "SA", "SU"];
 
-export const TimeContainer = ({ work }) => {      
-    return (
-        <ul>
-            <TimeList>
-                <Time>Time:</Time>
-                {work ? work.map(({ isOpen, from, to }, index) => (index === date.getDay() && isOpen) &&
-                    <p key={index}>{from} - {to}</p>) : <p>{"Closed"}</p>
-                }
+export const TimeContainer = ({ work }) => {   
+    const [visible, setVisible] = useState(false);
 
-                <ul>
+    const toggle = () => {
+        setVisible(state => !state);
+    }
+
+    return (
+            <TimeList>
+                
+            <button onClick={toggle}>
+                <Time>
+                    <span>Time:</span>
+                    {work ? work.map(({ isOpen, from, to }, index) => (index === date.getDay() && isOpen) &&
+                            <span key={index}>{from} - {to}</span>) : <span>{"Closed"}</span>
+                        }
+                    </Time>
+                </button>
+                
+
+                {visible && <ul>
                     {work ? work.map(({ isOpen, from, to }, index) => isOpen ?
                         <LiStyled key={index} index={index}><ListWeek>{week[index]}</ListWeek><ListTime>{from}- {to}</ListTime></LiStyled> :
                         <LiStyled key={index} index={index}><ListWeek>{week[index]}</ListWeek><ListTime>Closed</ListTime></LiStyled>) : <li>{"We don't know, yet"}</li>
                     }
-                </ul>
+                </ul>}
 
             </TimeList>
-        </ul>
     )
 }  
