1d7c02c64a0544787d5fe0a98428b27490e87947
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2023-02-11 22:06:46 +0200

    fix datepicker, add notifications

--- src/components/Notices/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/Notices/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,73 +1,103 @@
-import { 
-    NoticeCard,
-    HeartBtn,
-    ButtonWrapper,
-    HeartIcon,
-    DeleteIcon,
-    Category,
-    CardImg,
-    CardTitle,
-    CardTextContainer,
-    StringWrapper,
-    StringTitle,
-    StringText,
-    Button,
-    ContentWrapper } from "./NoticeCategoryItem.styled";
-import { useAuth } from "hooks/useAuth";
-import { useDispatch } from "react-redux";
-import { noticesOperations } from "redux/notices";
-import { authOperations } from "redux/auth";
-import getAge from "./getAge";
+import {
+  NoticeCard,
+  HeartBtn,
+  ButtonWrapper,
+  HeartIcon,
+  DeleteIcon,
+  Category,
+  CardImg,
+  CardTitle,
+  CardTextContainer,
+  StringWrapper,
+  StringTitle,
+  StringText,
+  Button,
+  ContentWrapper,
+} from './NoticeCategoryItem.styled';
+import { useAuth } from 'hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { noticesOperations } from 'redux/notices';
+import { authOperations } from 'redux/auth';
+import getAge from './getAge';
+import Notiflix from 'notiflix/build/notiflix-notify-aio';
 
 export const NoticeCategoryItem = ({ notice, learnMore }) => {
-    const { category, photoUrl, title, breed, location, price, birthday, _id, owner } = notice;
-    const {user} = useAuth();
-    const dispatch = useDispatch();
-    const age = getAge(birthday);
+  const {
+    category,
+    photoUrl,
+    title,
+    breed,
+    location,
+    price,
+    birthday,
+    _id,
+    owner,
+  } = notice;
+  const { user, isLoggedIn } = useAuth();
+  const dispatch = useDispatch();
+  const age = getAge(birthday);
 
-    const isOwner = user._id === owner;
-    const favorite = user.favorites?.includes(_id) ? 1 : 0;
+  const isOwner = user._id === owner;
+  const favorite = user.favorites?.includes(_id) ? 1 : 0;
 
-    const handleDelete = () => {
-        dispatch(noticesOperations.remove(_id))
-    }
+  const handleDelete = () => {
+    dispatch(noticesOperations.remove(_id));
+  };
 
-    const toggleFavorites = () => {
-        dispatch(authOperations.favorites(_id))
-    }
+  const toggleFavorites = () => {
+    isLoggedIn
+      ? dispatch(authOperations.favorites(_id))
+      : Notiflix.Notify.failure(
+          `You must log in to add Notice in favorite list`,
+          {
+            timeout: 3000,
+          }
+        );
+  };
 
-    return (
-        <NoticeCard>
-            <Category>{category}</Category>
-            {!isOwner
-             && <HeartBtn className="heart"type="button" onClick={toggleFavorites}>{<HeartIcon favorite={favorite} />}</HeartBtn>}
-            <CardImg src={photoUrl} alt={title}/>
-            <ContentWrapper>
-                <CardTextContainer>
-                    <CardTitle>{title}</CardTitle>
-                    <StringWrapper>
-                        <StringTitle>Breed:</StringTitle>
-                        <StringText>{breed}</StringText>
-                    </StringWrapper>
-                    <StringWrapper>
-                        <StringTitle>Place:</StringTitle>
-                        <StringText>{location}</StringText>
-                    </StringWrapper>
-                    <StringWrapper>
-                        <StringTitle>Age:</StringTitle>
-                        <StringText>{age}</StringText>
-                    </StringWrapper>
-                    {price && <StringWrapper>
-                        <StringTitle>Price:</StringTitle>
-                        <StringText>{price}</StringText>
-                    </StringWrapper>}
-                </CardTextContainer>
-                <ButtonWrapper>
-                        <Button type="button" onClick={() => learnMore(_id)}>Learn more</Button>
-                        {isOwner
-                        && <Button type="button" onClick={handleDelete}>Delete<DeleteIcon /></Button> }
-                </ButtonWrapper>
-            </ContentWrapper>
-        </NoticeCard>
-    )
-}
+  return (
+    <NoticeCard>
+      <Category>{category}</Category>
+      {!isOwner && (
+        <HeartBtn className="heart" type="button" onClick={toggleFavorites}>
+          {<HeartIcon favorite={favorite} />}
+        </HeartBtn>
+      )}
+      <CardImg src={photoUrl} alt={title} />
+      <ContentWrapper>
+        <CardTextContainer>
+          <CardTitle>{title}</CardTitle>
+          <StringWrapper>
+            <StringTitle>Breed:</StringTitle>
+            <StringText>{breed}</StringText>
+          </StringWrapper>
+          <StringWrapper>
+            <StringTitle>Place:</StringTitle>
+            <StringText>{location}</StringText>
+          </StringWrapper>
+          <StringWrapper>
+            <StringTitle>Age:</StringTitle>
+            <StringText>{age}</StringText>
+          </StringWrapper>
+          {price && (
+            <StringWrapper>
+              <StringTitle>Price:</StringTitle>
+              <StringText>{price}</StringText>
+            </StringWrapper>
+          )}
+        </CardTextContainer>
+        <ButtonWrapper>
+          <Button type="button" onClick={() => learnMore(_id)}>
+            Learn more
+          </Button>
+          {isOwner && (
+            <Button type="button" onClick={handleDelete}>
+              Delete
+              <DeleteIcon />
+            </Button>
+          )}
+        </ButtonWrapper>
+      </ContentWrapper>
+    </NoticeCard>
+  );
+};
--- src/redux/notices/noticesOperations.js
+++ src/redux/notices/noticesOperations.js
@@ -1,77 +1,91 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import Notiflix from 'notiflix/build/notiflix-notify-aio';
 import * as api from '../../services/notices';
 
-const add = createAsyncThunk('notices/add', async (notice, thunkApi) => {
-    try {
-        const {data} = await api.add(notice);
-        return data;
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
-    }
-}) 
+Notiflix.Notify.init({ position: 'left-bottom' });
 
-const getAll = createAsyncThunk('notices/getAll', async (category, thunkApi) => {
-    try {
-        const {filter} = thunkApi.getState().filter;
-        const {data} = await api.getAll(category, filter);
-        return data;
-        
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
-    }
+const add = createAsyncThunk('notices/add', async (notice, thunkApi) => {
+  try {
+    const { data } = await api.add(notice);
+    Notiflix.Notify.success('ad successfully created', {
+      timeout: 3000,
+    });
+    return data;
+  } catch ({ result }) {
+    Notiflix.Notify.failure(`something goe wrong ${result.data.message}`, {
+      timeout: 3000,
+    });
+    return thunkApi.rejectWithValue(result.data.message);
+  }
 });
 
-const getOne = createAsyncThunk('notices/getOne', async (id, thunkApi) => {
+const getAll = createAsyncThunk(
+  'notices/getAll',
+  async (category, thunkApi) => {
     try {
-        const result = await api.getOne(id);
-        return result;
-
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
+      const { filter } = thunkApi.getState().filter;
+      const { data } = await api.getAll(category, filter);
+      return data;
+    } catch ({ result }) {
+      return thunkApi.rejectWithValue(result.data.message);
     }
+  }
+);
+
+const getOne = createAsyncThunk('notices/getOne', async (id, thunkApi) => {
+  try {
+    const result = await api.getOne(id);
+    return result;
+  } catch ({ result }) {
+    return thunkApi.rejectWithValue(result.data.message);
+  }
 });
 
 const getOwn = createAsyncThunk('notices/getOwn', async (_, thunkApi) => {
-    try {
-        const {data} = await api.getOwn();
-        return data;
-        
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
-    }
+  try {
+    const { data } = await api.getOwn();
+    return data;
+  } catch ({ result }) {
+    return thunkApi.rejectWithValue(result.data.message);
+  }
 });
 
-const getFavorites = createAsyncThunk('notices/getFavorites', async (_, thunkApi) => {
+const getFavorites = createAsyncThunk(
+  'notices/getFavorites',
+  async (_, thunkApi) => {
     try {
-        const {data} = await api.getFavorites();
-        return data;
-
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
+      const { data } = await api.getFavorites();
+      return data;
+    } catch ({ result }) {
+      return thunkApi.rejectWithValue(result.data.message);
     }
-});
+  }
+);
 
 const remove = createAsyncThunk('notices/remove', async (id, thunkApi) => {
-    try {
-        await api.remove(id);
-        return id;
-    } catch ({result}) {
-        return thunkApi.rejectWithValue(result.data.message)
-    }
-})
+  try {
+    await api.remove(id);
+    return id;
+  } catch ({ result }) {
+    return thunkApi.rejectWithValue(result.data.message);
+  }
+});
 
-const setCategory = createAsyncThunk('notice/category', async (category, thunkApi) => {
+const setCategory = createAsyncThunk(
+  'notice/category',
+  async (category, thunkApi) => {
     return category;
-})
+  }
+);
 
 const noticesOperations = {
-    getAll,
-    getOne,
-    getOwn,
-    getFavorites,
-    add,
-    remove,
-    setCategory
+  getAll,
+  getOne,
+  getOwn,
+  getFavorites,
+  add,
+  remove,
+  setCategory,
 };
 
 export default noticesOperations;
