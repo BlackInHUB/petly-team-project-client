77d5578ce11acd24fa9d4cb4f0549591961417c1
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2023-02-12 19:23:45 +0200

    profile data added pets and notices

--- src/components/UserData/UserDataItem/UserDataItem.jsx
+++ src/components/UserData/UserDataItem/UserDataItem.jsx
@@ -1,64 +1,93 @@
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { authOperations } from '../../../redux/auth'
-import { UserDataItemWrapper, UserDataItemLabel, UserDataItemInputBtnWrapper,
-    UserDataItemInput, UserDataItemBtn, PensilStyle, CheckMarkStyle } from './UserDataItem.styled'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from '../../../redux/auth';
+import {
+  UserDataItemWrapper,
+  UserDataItemLabel,
+  UserDataItemInputBtnWrapper,
+  UserDataItemInput,
+  UserDataItemBtn,
+  PensilStyle,
+  CheckMarkStyle,
+} from './UserDataItem.styled';
 
-export const UserDataItem = ({name, label, type, defaultValue, active, setActive}) => {
-    const dispatch = useDispatch();
-    
-    const [inputValue, setInputValue] = useState(defaultValue);
+export const UserDataItem = ({
+  name,
+  label,
+  type,
+  defaultValue,
+  active,
+  setActive,
+  profile,
+}) => {
+  const dispatch = useDispatch();
 
-    const handleChange = (e) => {
-        const {name, value} = (e.currentTarget)
-        
-            if (name === 'name'){
-                setInputValue(value)
-            }
-            if (name === 'email'){
-                setInputValue(value)
-                
-            }
-            else if (name === 'birthday'){
-                setInputValue(value)
-            }
-            else if (name === 'phone'){
-                setInputValue(value)
-            }
-            else if (name === 'city'){
-                setInputValue(value)
-            }
+  const [inputValue, setInputValue] = useState(defaultValue);
+
+  console.log(defaultValue);
+  console.log(inputValue);
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    if (name === 'name') {
+      setInputValue(value);
     }
-
-    const handleSubmit = (name) => {
-        setActive('')
-        dispatch(authOperations.update({[name]: inputValue}))
-    }    
-
-    const activeHandleClick = (name) => {
-        setActive(name)
+    if (name === 'email') {
+      setInputValue(value);
+    } else if (name === 'birthday') {
+      setInputValue(value);
+    } else if (name === 'phone') {
+      setInputValue(value);
+    } else if (name === 'city') {
+      setInputValue(value);
     }
+  };
 
-    return(
-        <>
-        <UserDataItemWrapper>
-                 <UserDataItemLabel htmlFor={name}>{label}</UserDataItemLabel>
-                 <UserDataItemInputBtnWrapper>
-                     <UserDataItemInput value={inputValue} onChange={handleChange} active={active === name} disabled={active !== name} type={type} name={name} id={name}/> 
+  const handleSubmit = name => {
+    setActive('');
+    dispatch(authOperations.update({ [name]: inputValue }));
+  };
 
-                    {active === name ? 
-                        <UserDataItemBtn ><CheckMarkStyle onClick={() => handleSubmit(name)}/></UserDataItemBtn> :
-                        <UserDataItemBtn disabled={active && active !== name} onClick={() => activeHandleClick(name)}><PensilStyle /></UserDataItemBtn>
-                    }
+  const activeHandleClick = name => {
+    setActive(name);
+  };
 
-                    {/* <UserDataItemBtn>
+  return (
+    <>
+      <UserDataItemWrapper>
+        <UserDataItemLabel htmlFor={name}>{label}</UserDataItemLabel>
+        <UserDataItemInputBtnWrapper>
+          <UserDataItemInput
+            value={!profile ? inputValue : defaultValue}
+            onChange={handleChange}
+            active={active === name}
+            disabled={active !== name}
+            type={type}
+            name={name}
+            id={name}
+          />
+          {!profile &&
+            (active === name ? (
+              <UserDataItemBtn>
+                <CheckMarkStyle onClick={() => handleSubmit(name)} />
+              </UserDataItemBtn>
+            ) : (
+              <UserDataItemBtn
+                disabled={active && active !== name}
+                onClick={() => activeHandleClick(name)}
+              >
+                <PensilStyle />
+              </UserDataItemBtn>
+            ))}
+
+          {/* <UserDataItemBtn>
                         {active === name ?
                         <CheckMarkStyle onClick={() => handleSubmit(name)}/> :
                         <PensilStyle disabled={active && active !== name} onClick={() => activeHandleClick(name)}/>}
                     </UserDataItemBtn> */}
-                    
-                 </UserDataItemInputBtnWrapper>                 
-         </UserDataItemWrapper>
-     </>
-    )
-}
+        </UserDataItemInputBtnWrapper>
+      </UserDataItemWrapper>
+    </>
+  );
+};
